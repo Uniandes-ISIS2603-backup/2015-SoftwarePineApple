54ef601b0cee8e45a16ba3b0eb4b975718a1e45f
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2015-03-04 17:58:58 -0500

    Ejemplo de parte relativamente funcional, revisar proveedor.

--- eckotur_web/public_html/src/modules/provider/provider.ctrl.js
+++ eckotur_web/public_html/src/modules/provider/provider.ctrl.js
@@ -1,7 +1,35 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(function () {
+	var app = angular.module('providerModule');
+	app.controller('providerCtrl', ['$scope', 'CRUDUtils', 'provider.context', function ($scope, CRUDUtils, context) {
+			this.url = context;
+			CRUDUtils.extendCtrl(this, $scope);
+			this.homeMode=true;
+                        /*
+                        this.showOffers = function(){
+                            this.url='offer.context';
+                            CRUDUtils.extendCtrl(this, $scope);
+                            alert($scope.records.length);
+                            this.fetchRecords();
+                            this.offersMode=true;
+                            this.salesMode=false;
+                        }*/
+                        this.showSales = function(){
+                            var earnings=0;
+                            this.registerSales();
+                            for(var i=0;i<$scope.currentRecord.sales.length;i++){
+                                earnings+=$scope.currentRecord.sales[i].price;
+                            }
+                            document.getElementById("totalOfEarnings").innerHTML="<label>Total of earnings: "+earnings+"</label>"
+                            this.salesMode=true;
+                            this.offersMode=false;
+                        }
+                        this.registerSales = function(){
+                            $scope.currentRecord.sales=[{id:'1',offer:"Viaje al centro de la tierra",buyer:"Julio Pepe",quantity:"1",price:"$100",date:'01/03/2015'},
+                            {id:'1',offer:"Viaje al centro de la tierra",buyer:"Julian Verne",quantity:"2",price:"$200",date:'04/03/2015'},
+                            {id:'2',offer:"Caída libre en El Salto del Ángel",buyer:"Pepe Pérez",quantity:"2",price:"$1000",date:'02/03/2015'},
+                            {id:'3',offer:"Ida al Amazonas",buyer:"Andrés Angelino",quantity:"3",price:"$200",date:'01/03/2015'}];
+                        }
+		}]);
+})();
 
 
--- eckotur_web/public_html/src/shared/crud/crud.svc.js
+++ eckotur_web/public_html/src/shared/crud/crud.svc.js
@@ -13,22 +13,19 @@
                                 $scope.sellsRecords=[];
                                 $scope.currentSellRecord={};
                                 $scope.offersRecords=[];
-                                $scope.currentOfferRecord={};*/
-                                
+                                $scope.currentOfferRecord={};
 				this.editMode = false;
                                 this.homeMode = false;
+                                this.responseMode = false;
+                                */
+                               
 				this.fetchRecords = function () {
-					/*var self = this;*/
 					this.api.getList().then(function (data) {
 						$scope.records = data;
 						$scope.currentRecord = {};
-						/*self.editMode = false;
-                                                self.registerMode = false;*/
 					});
 				};
 				this.createRecord = function () {
-					this.editMode = true;
-                                        this.responseMode = false;
 					$scope.currentRecord = {};
 				};
 				this.saveRecord = function () {
@@ -49,19 +46,17 @@
 						self.fetchRecords();
 					});
 				};
-                                
-                                
-                                
 				this.editRecord = function (record) {
 					$scope.currentRecord = RestAngular.copy(record);
-					this.editMode = true;
-                                        this.homeMode = false;
 				};
+                                
+                                
+                                /*
                                 this.fetchSellsRecords = function (){
 					var self = this;
 					this.api.getList().then(function (data) {
 						/*$scope.sellsRecords = data;
-						$scope.currentSellRecord = {};*/
+						$scope.currentSellRecord = {};
                                                 $scope.records = data;
                                                 $scope.currentRecord = {};
 						self.sellsMode = true;
@@ -79,7 +74,7 @@
                                     var self = this;
 					this.api.getList().then(function (data) {
 						/*$scope.offersRecords = data;
-						$scope.currentOfferRecord = {};*/
+						$scope.currentOfferRecord = {};
                                                 $scope.records = data;
                                                 $scope.currentRecord = {};
 						self.sellsMode = true;
@@ -97,7 +92,7 @@
                                 };
                                 this.deleteOffer = function(record){
                                     
-                                };
+                                };*/
 			}
 			;
 			return {
