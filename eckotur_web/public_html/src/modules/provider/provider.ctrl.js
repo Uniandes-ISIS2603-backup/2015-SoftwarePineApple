618d796c281455cc968c002da881332c57a4db38
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2015-03-04 21:45:25 -0500

    Crud básico de proveedor
    Buena parte del proveedor realizada.

--- eckotur_web/public_html/src/modules/provider/provider.ctrl.js
+++ eckotur_web/public_html/src/modules/provider/provider.ctrl.js
@@ -4,31 +4,36 @@
 			this.url = context;
 			CRUDUtils.extendCtrl(this, $scope);
 			this.homeMode=true;
-                        /*
+                        $scope.currentRecord.sales=[];
+                        $scope.currentRecord.offers=[];
+                        this.saveOffer = function(pOffer){
+                            pOffer.nSales=0;
+                            var offer = angular.copy(pOffer);
+                            $scope.currentRecord.offers.push(offer);
+                            this.showOffers();
+                            document.getElementById("createOfferForm").reset();
+                        };
                         this.showOffers = function(){
-                            this.url='offer.context';
-                            CRUDUtils.extendCtrl(this, $scope);
-                            alert($scope.records.length);
-                            this.fetchRecords();
                             this.offersMode=true;
+                            this.createOfferMode=false;
                             this.salesMode=false;
-                        }*/
+                        };
                         this.showSales = function(){
-                            var earnings=0;
                             this.registerSales();
-                            for(var i=0;i<$scope.currentRecord.sales.length;i++){
-                                earnings+=$scope.currentRecord.sales[i].price;
-                            }
-                            document.getElementById("totalOfEarnings").innerHTML="<label>Total of earnings: "+earnings+"</label>"
                             this.salesMode=true;
                             this.offersMode=false;
-                        }
+                        };
+                        this.createOffer = function(){
+                            this.createOfferMode=true;
+                            this.salesMode=false;
+                            this.offersMode=false;
+                        };
                         this.registerSales = function(){
                             $scope.currentRecord.sales=[{id:'1',offer:"Viaje al centro de la tierra",buyer:"Julio Pepe",quantity:"1",price:"$100",date:'01/03/2015'},
                             {id:'1',offer:"Viaje al centro de la tierra",buyer:"Julian Verne",quantity:"2",price:"$200",date:'04/03/2015'},
                             {id:'2',offer:"Caída libre en El Salto del Ángel",buyer:"Pepe Pérez",quantity:"2",price:"$1000",date:'02/03/2015'},
                             {id:'3',offer:"Ida al Amazonas",buyer:"Andrés Angelino",quantity:"3",price:"$200",date:'01/03/2015'}];
-                        }
+                        };
 		}]);
 })();
 
