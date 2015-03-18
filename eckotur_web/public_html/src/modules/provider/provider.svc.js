2bc94d29fe0daac9018d8e5bfe67c773ca0d4047
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2015-03-18 13:51:26 -0500

    Cambios al proveedor

--- eckotur_web/public_html/src/modules/provider/provider.svc.js
+++ eckotur_web/public_html/src/modules/provider/provider.svc.js
@@ -2,8 +2,28 @@
     var providerModule = angular.module('providerModule');
     
     providerModule.service('providerService', ['CRUDBase','provider.context', function(CRUDBase, context){
-            this.url = context;
-            CRUDBase.extendService(this);
+        this.url = context;
+        CRUDBase.extendService(this);
+        
+        this.getOffers = function(){
+            return this.api.customGET('offers');
+        };
+        
+        this.getSales = function(){
+            return this.api.customGET('sales');
+        };
+        
+        this.getNews = function(){
+            return this.api.customGET('news');
+        };
+        
+        this.getMessages = function(){
+            return this.api.customGET('messages');
+        };
+        
+        this.getReports = function(){
+            return this.api.customGET('reports');
+        };
     }]);
 })();
 
