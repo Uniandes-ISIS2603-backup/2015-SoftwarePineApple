a2cf2fedbad64dd148cc20e3aa4564c5053aeaa2
Daniel Torres <d.torres11@uniandes.edu.co>
2015-03-25 16:46:24 -0500

    Revert "controlador offer"
    This reverts commit f894a9fb26e16b580d96dbadf92a2d3c3143cc0a.

--- FrontEnd/eckotur_web/public_html/src/modules/offer/offer.mod.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var providerModule = angular.module('offerModule', ['CrudModule', 'MockModule']);
-
-    providerModule.constant('offer.context', 'offers');
-
-    providerModule.config(['offer.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-        urlsProvider.registerUrl(context);
-    }]);
-})();
