9ba157e41204b5e79840a2a77ea277270f5f0066
Felipe Campos <felipe8ap@gmail.com>
2015-03-25 23:26:07 -0500

    Conexion entre los html completa+bought entity
    bought entity completa
    todas las paginas de eckotur ya estan conectadas entre si

--- BackEnd/Eckotur.logic/src/main/java/com/pineapple/eckotur/client/logic/entity/BoughtEntity.java
+++ BackEnd/Eckotur.logic/src/main/java/com/pineapple/eckotur/client/logic/entity/BoughtEntity.java
@@ -4,13 +4,64 @@
  * and open the template in the editor.
  */
 package com.pineapple.eckotur.client.logic.entity;
+import java.sql.Date;
 
-/**
- *
- * @author estudiante
- */
-public class BoughtEntity
-{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class BoughtEntity {
+
+    @Id
+    @GeneratedValue(generator = "Message")
     
-    
+	
+	private Long userId;
+	
+	private Long providerId;
+	
+	private Long date;
+	
+	private Long transactionId;
+	
+	private Long offerId;
+	
+	public Long getUserId(){
+		return userId;
+	}
+	public Long getProviderId(){
+		return providerId;
+	}
+	public Long getDate(){
+		return date;
+	}
+	public Long getTransactionId(){
+		return transactionId;
+	}
+	public Long getOfferId(){
+		return offerId;
+	}
+	
+	public void setUserId(Long id){
+		this.userId=id;
+	}
+	
+	public void setProviderId(Long IProviderId){
+		this.providerId=IProviderId;
+	}
+	
+	public void setDate(Long IDate){
+		this.date=IDate;
+	}
+	
+	public void setTransactionId(Long ITransactionId){
+		this.transactionId=ITransactionId;
+	}
+	
+	public void setMessage(Long IOfferId){
+		this.offerId=IOfferId;
+	}
+	
+
 }
