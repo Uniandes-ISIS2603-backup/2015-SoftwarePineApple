60cfdaef4b23e9a5a5da6cdf9679c7e246e84a32
scorrea12 <s.correa12@uniandes.edu.co>
2015-04-21 10:36:58 -0500

    Corregir el app.js

--- BackEnd Con Front/Eckotur.service/src/main/webapp/src/app.js
+++ BackEnd Con Front/Eckotur.service/src/main/webapp/src/app.js
@@ -3,47 +3,7 @@
 	var mainApp = angular.module('mainApp', ['ngRoute', 'accomodationModule', 'adminModule', 'catalogModule', 'clientModule', 'ecoTourModule', 'foodPlanModule', 'providerModule','loginModule']);
         var mainApp2 = angular.module('mainApp2', ['ngRoute', 'accomodationModule', 'adminModule', 'catalogModule', 'clientModule', 'ecoTourModule', 'foodPlanModule', 'providerModule','loginModule']);
 
-	var accomodationModule = angular.module('accomodationModule', ['CrudModule', 'MockModule']);
-	accomodationModule.constant('accomodation.context', 'accomodation');
-	accomodationModule.config(['accomodation.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-			urlsProvider.registerUrl(context);
-		}]);
-        
-        var adminModule = angular.module('adminModule', ['CrudModule', 'MockModule']);
-	adminModule.constant('admin.context', 'admin');
-	adminModule.config(['admin.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-			urlsProvider.registerUrl(context);
-		}]);    
-        var catalogModule = angular.module('catalogModule', ['CrudModule', 'MockModule']);
-	catalogModule.constant('catalog.context', 'catalog');
-	catalogModule.config(['catalog.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-			urlsProvider.registerUrl(context);
-		}]);
-        var clientModule = angular.module('clientModule', ['CrudModule', 'MockModule']);
-	clientModule.constant('client.context', 'client');
-	clientModule.config(['client.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-			urlsProvider.registerUrl(context);
-		}]);
-        var ecoTourModule = angular.module('ecoTourModule', ['CrudModule', 'MockModule']);
-	ecoTourModule.constant('ecoTour.context', 'ecoTour');
-	ecoTourModule.config(['ecoTour.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-			urlsProvider.registerUrl(context);
-		}]);
-        var foodPlanModule = angular.module('foodPlanModule', ['CrudModule', 'MockModule']);
-	foodPlanModule.constant('foodPlan.context', 'foodPlan');
-	foodPlanModule.config(['foodPlan.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-			urlsProvider.registerUrl(context);
-		}]);
-        var providerModule = angular.module('providerModule', ['CrudModule', 'MockModule']);
-	providerModule.constant('provider.context', 'provider');
-	providerModule.config(['provider.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-			urlsProvider.registerUrl(context);
-		}]);
-            var loginModule = angular.module('loginModule', ['CrudModule', 'MockModule']);
-	loginModule.constant('login.context', 'login');
-	loginModule.config(['login.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-			urlsProvider.registerUrl(context);
-		}]);
+
 
 	mainApp.config(['$routeProvider', function ($routeProvider) {
 			$routeProvider.when('/accomodation', {
