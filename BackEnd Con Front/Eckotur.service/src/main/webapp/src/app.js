1e9fe459b133d2387ac5809e5e9366c71fa3340f
Juan David Cruz Serrano <jd.cruz12@uniandes.edu.co>
2015-04-29 11:21:35 -0500

    Mod Js Arreglado
    Mod Js Arreglado

--- BackEnd Con Front/Eckotur.service/src/main/webapp/src/app.js
+++ BackEnd Con Front/Eckotur.service/src/main/webapp/src/app.js
@@ -1,8 +1,7 @@
 (function () {
 
-	var mainApp = angular.module('mainApp', ['ngRoute',  'catalogModule', 'clientModule',  'providerModule','loginModule']);
-        var mainApp2 = angular.module('mainApp2', ['ngRoute', 'catalogModule', 'clientModule', 'providerModule','loginModule']);
-
+	var mainApp = angular.module('mainApp', ['ngRoute',  'catalogModule', 'clientModule',  'providerModule','loginModule', 'salesModule']);
+        
 
 
 	mainApp.config(['$routeProvider', function ($routeProvider) {
@@ -16,28 +15,16 @@
                                 templateUrl: 'src/modules/provider/provider.tpl.html'
                         }).otherwise('/');
                         $routeProvider.when('/login', {
-                                templateUrl: 'src/login.html'
+                                templateUrl: '../login.html'
+                               
+                        }).otherwise('/');
+                        $routeProvider.when('/sales', {
+                                templateUrl: '../pages/tables.html'
                                
                         }).otherwise('/');
                        
-		}]);
-mainApp2.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/catalog', {
-                                templateUrl: 'src/modules/catalog/catalog.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/client', {
-                                templateUrl: 'src/modules/client/client.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/provider', {
-                                templateUrl: 'src/modules/provider/provider.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/login', {
-                                templateUrl: 'src/login.html'
-                        }).otherwise('/');
                        
 		}]);
-	
-            
       
 })();
 
