8758bcc2cd07e955916622653eccce009e607e2e
Juan David Cruz Serrano <jd.cruz12@uniandes.edu.co>
2015-04-29 11:14:59 -0500

    Arregle APP.JS
    Arregle APP.JS

--- BackEnd Con Front/Eckotur.service/src/main/webapp/src/app.js
+++ BackEnd Con Front/Eckotur.service/src/main/webapp/src/app.js
@@ -1,28 +1,16 @@
 (function () {
 
-	var mainApp = angular.module('mainApp', ['ngRoute', 'accomodationModule', 'adminModule', 'catalogModule', 'clientModule', 'ecoTourModule', 'foodPlanModule', 'providerModule','loginModule']);
-        var mainApp2 = angular.module('mainApp2', ['ngRoute', 'accomodationModule', 'adminModule', 'catalogModule', 'clientModule', 'ecoTourModule', 'foodPlanModule', 'providerModule','loginModule']);
+	var mainApp = angular.module('mainApp', ['ngRoute',  'catalogModule', 'clientModule',  'providerModule','loginModule']);
+        var mainApp2 = angular.module('mainApp2', ['ngRoute', 'catalogModule', 'clientModule', 'providerModule','loginModule']);
 
 
 
 	mainApp.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/accomodation', {
-				templateUrl: 'src/modules/accomodation/accomodation.tpl.html'
-			}).otherwise('/');
-                        $routeProvider.when('/admin', {
-                                templateUrl: 'src/modules/admin/admin.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/catalog', {
+			$routeProvider.when('/catalog', {
                                 templateUrl: 'src/modules/catalog/catalog.tpl.html'
                         }).otherwise('/');
                         $routeProvider.when('/client', {
                                 templateUrl: 'src/modules/client/client.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/ecoTour', {
-                                templateUrl: 'src/modules/ecoTour/ecoTour.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/foodPlan', {
-                                templateUrl: 'src/modules/foodPlan/foodPlan.tpl.html'
                         }).otherwise('/');
                         $routeProvider.when('/provider', {
                                 templateUrl: 'src/modules/provider/provider.tpl.html'
@@ -34,23 +22,11 @@
                        
 		}]);
 mainApp2.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/accomodation', {
-				templateUrl: 'src/modules/accomodation/accomodation.tpl.html'
-			}).otherwise('/');
-                        $routeProvider.when('/admin', {
-                                templateUrl: 'src/modules/admin/admin.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/catalog', {
+			$routeProvider.when('/catalog', {
                                 templateUrl: 'src/modules/catalog/catalog.tpl.html'
                         }).otherwise('/');
                         $routeProvider.when('/client', {
                                 templateUrl: 'src/modules/client/client.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/ecoTour', {
-                                templateUrl: 'src/modules/ecoTour/ecoTour.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/foodPlan', {
-                                templateUrl: 'src/modules/foodPlan/foodPlan.tpl.html'
                         }).otherwise('/');
                         $routeProvider.when('/provider', {
                                 templateUrl: 'src/modules/provider/provider.tpl.html'
