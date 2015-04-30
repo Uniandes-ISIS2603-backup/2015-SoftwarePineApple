7ca8ec203c57b7c4fa59bb8e0b02f7fad68fa841
Felipe Campos <felipe8ap@gmail.com>
2015-04-30 11:39:00 -0500

    Modificacion OfferService UPDATE COMPLETO
    TODOS LOS SERVICIOS OFFER ESTAS COMPLETOS Y FUNCIONALES

--- BackEnd Con Front/Eckotur.service/src/main/java/com/pineapple/eckotur/service/OfferService.java
+++ BackEnd Con Front/Eckotur.service/src/main/java/com/pineapple/eckotur/service/OfferService.java
@@ -8,6 +8,8 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,18 +23,31 @@ import javax.ws.rs.core.MediaType;
 public class OfferService {
 
     @Inject
-    protected IOfferLogic catalogLogicService;
+    protected IOfferLogic OfferLogicService;
 
     
     @GET
     public OfferPageDTO getOffer(@QueryParam("page") Integer page, @QueryParam("maxRecords") Integer maxRecords) {
-        return catalogLogicService.getOffers(page, maxRecords);
+        return OfferLogicService.getOffers(page, maxRecords);
     }
 
     @GET
     @Path("{id}")
     public OfferDTO getOffer(@PathParam("id") Long id) {
-        return catalogLogicService.getOffer(id);
+        return OfferLogicService.getOffer(id);
     }
 
+    @POST
+    public void createOffer (OfferDTO offer)
+    {
+        OfferLogicService.createOffer(offer);
+    }
+    
+    @PUT
+    @Path("{id}")
+    public void updateOffer (@PathParam("id")Long id,OfferDTO offer)
+    {
+        OfferLogicService.updateOffer(offer);
+    }
 }
+
