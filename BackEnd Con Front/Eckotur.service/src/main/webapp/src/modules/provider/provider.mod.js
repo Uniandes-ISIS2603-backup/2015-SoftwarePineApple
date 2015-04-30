d920fe50a94cbddb7d8059bc6073220e4085e5f1
Juan David Cruz Serrano <jd.cruz12@uniandes.edu.co>
2015-04-30 10:27:49 -0500

    ARREGLOS MOCK
    ARREGLOS MOCK

--- BackEnd Con Front/Eckotur.service/src/main/webapp/src/modules/provider/provider.mod.js
+++ BackEnd Con Front/Eckotur.service/src/main/webapp/src/modules/provider/provider.mod.js
@@ -1,7 +1,7 @@
 (function () {
-    var providerModule = angular.module('providerModule', ['CrudModule', 'MockModule', 'salesModule']);
+    var providerModule = angular.module('providerModule', ['CrudModule', 'MockModule']);
 
-   providerModule.constant('provider.skipMock', true);
+   providerModule.constant('provider.skipMock', false);
 
      providerModule.config(['provider.context', 'MockModule.urlsProvider','provider.skipMock', function (context, urlsProvider, skipMock) {
            urlsProvider.registerUrl(context, skipMock);         }]);
--- BackEnd Con Front/Eckotur.service/target/Eckotur.service-0.0.1-SNAPSHOT/src/app.js
+++ BackEnd Con Front/Eckotur.service/target/Eckotur.service-0.0.1-SNAPSHOT/src/app.js
@@ -1,29 +1,20 @@
 (function () {
 
-	var mainApp = angular.module('mainApp', ['ngRoute',  'catalogModule', 'clientModule',  'providerModule','loginModule', 'salesModule']);
+	var mainApp = angular.module('mainApp', ['ngRoute',  'catalogModule', 'clientModule',  'providerModule','loginModule', 'salesModule', 'offerModule']);
         
 
 
 	mainApp.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/catalog', {
-                                templateUrl: 'src/modules/catalog/catalog.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/client', {
-                                templateUrl: 'src/modules/client/client.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/provider', {
-                                templateUrl: 'src/modules/provider/provider.tpl.html'
-                        }).otherwise('/');
-                        $routeProvider.when('/login', {
-                                templateUrl: '../login.html'
-                               
-                        }).otherwise('/');
-                        $routeProvider.when('/sales', {
-                                templateUrl: '../pages/tables.html'
-                               
-                        }).otherwise('/');
-                       
-                       
+			
+		}]);
+            
+            
+            var mainApp2 = angular.module('mainApp2', ['providerModule', 'offerModule']);
+        
+
+
+	mainApp2.config(['$routeProvider', function ($routeProvider) {
+			
 		}]);
       
 })();
