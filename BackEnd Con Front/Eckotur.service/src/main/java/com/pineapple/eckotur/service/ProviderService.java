e14faac2dd4a549ad04df2660bac157baa5f7d05
Felipe Campos <felipe8ap@gmail.com>
2015-05-03 13:15:42 -0500

    Agregados servicios de provider para offers
    Se agregan los servicios de crear eliminar y modificar offers, segun
    mariana es correcto, aunque aumenta la complejidad

--- BackEnd Con Front/Eckotur.service/src/main/java/com/pineapple/eckotur/service/ProviderService.java
+++ BackEnd Con Front/Eckotur.service/src/main/java/com/pineapple/eckotur/service/ProviderService.java
@@ -2,11 +2,12 @@ package com.pineapple.eckotur.service;
 
 import com.pineapple.eckotur.message.logic.dto.MessageDTO;
 import com.pineapple.eckotur.news.logic.dto.NewsDTO;
-import com.pineapple.eckotur.offer.logic.dto.OfferDTO;
+import com.pineapple.eckotur.offer.logic.api.IOfferLogic;
 import com.pineapple.eckotur.provider.logic.api.IProviderLogic;
 import com.pineapple.eckotur.provider.logic.dto.ProviderDTO;
 import com.pineapple.eckotur.provider.logic.dto.ProviderPageDTO;
 import com.pineapple.eckotur.provider.logic.dto.SaleDTO;
+import com.pineapple.eckotur.offer.logic.dto.OfferDTO;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -28,18 +29,35 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class ProviderService {
 
+    /**
+     *
+     */
     @Inject
     protected IProviderLogic providerLogicService;
+    protected IOfferLogic offerLogicService;
 
     @POST
     public ProviderDTO createProvider(ProviderDTO provider) {
         return providerLogicService.createProvider(provider);
+    }
+    
+    @POST
+    public OfferDTO createOffer (OfferDTO offer)
+    {
+        return offerLogicService.createOffer(offer);
     }
 
     @DELETE
     @Path("{id}")
     public void deleteProvider(@PathParam("id") Long id) {
         providerLogicService.deleteProvider(id);
+    }
+    
+    @DELETE
+     @Path("{id}/offers")
+    public void deleteOffer (@PathParam("id")Long id, Long idOffer )
+    { 
+        offerLogicService.deleteOffer(idOffer);
     }
 
     @GET
@@ -56,6 +74,13 @@ public class ProviderService {
     @PUT
     public void updateProvider(@PathParam("id") Long id, ProviderDTO provider) {
         providerLogicService.updateProvider(provider);
+    }
+    
+     @PUT
+    @Path("{id}/offers")
+    public void updateOffer (@PathParam("id")Long id,OfferDTO offer)
+    {
+        offerLogicService.updateOffer(offer);
     }
     
     @GET
@@ -81,4 +106,5 @@ public class ProviderService {
     public List<MessageDTO> getProviderMessages(@PathParam("id") Long id){
         return providerLogicService.getProviderMessages(id);
     }
+    
 }
