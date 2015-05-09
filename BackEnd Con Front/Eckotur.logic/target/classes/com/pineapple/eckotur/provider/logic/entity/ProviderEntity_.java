7be22f941891bc917097dba5aba02f33d2f8b168
Felipe Campos <felipe8ap@gmail.com>
2015-05-08 22:12:40 -0500

    Minor Changes
    Builds independientes funcionan, en conjunto no.
    Se necesita arreglo a la dependencia en el servicio de crear oferta por
    parte del proveedor

--- BackEnd Con Front/Eckotur.logic/target/classes/com/pineapple/eckotur/provider/logic/entity/ProviderEntity_.java
+++ BackEnd Con Front/Eckotur.logic/target/classes/com/pineapple/eckotur/provider/logic/entity/ProviderEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2015-05-07T10:26:59")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2015-05-08T22:08:50")
 @StaticMetamodel(ProviderEntity.class)
 public class ProviderEntity_ { 
 
